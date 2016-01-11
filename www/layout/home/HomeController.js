89c823a67ab2f06c359a459892e1cd9e4e2c2b10
MPereira <mpereira132@hotmail.com>
2016-01-11 21:44:58 +0000

    #S07
    deploy to www

--- www/layout/home/HomeController.js
+++ www/layout/home/HomeController.js
@@ -1,10 +1,26 @@
-
 app
-    .controller('HomeController', ['$scope', '$location', function($scope, $location) {
-        $scope.move = false;
+    .controller('HomeController', ['$scope', '$location','$rootScope', function($scope, $location,$rootScope) {
 
-        $scope.navigate = function (path) {
-            $location.path(path);
+        // Feed Noticias
+        $scope.newsArray = [
+            {id: 0 , titulo: 'Feira Medieval', descricao:'No passado dia 8 de dezem-bro, feriado nacional, cumpriu-se mais uma edição do Mercado Medieval de Mesão Frio...', image: 'assets/news-tile-pics/news_tile_0.png'},
+            {id: 1 , titulo: 'Convenção do Concelho da Europa', descricao:'A violência sexual é um problema complexo, sensível e assustado-ramente generalizado...', image: ''},
+            {id: 2 , titulo: 'Uma aula diferente', descricao:'Há espetáculos que se dirigem especificamente ao público jovem...', image: 'assets/news-tile-pics/news_tile_2.png'},
+            {id: 3 , titulo: 'Prevenção do Cancro da mama', descricao:'Outubro é o mês internacional de Prevenção de Cancro da Mama...', image: 'assets/news-tile-pics/news_tile_3.png'},
+            {id: 4 , titulo: 'Marcha solidária', descricao:'Marcha Solidária a favor das Doenças Neurodegenerativas', image: 'assets/news-tile-pics/news_tile_4.png'},
+            {id: 5 , titulo: 'São Martinho', descricao:'“...cortou a capa que trazia ao meio dando uma parte ao pobre, …”', image: 'assets/news-tile-pics/news_tile_5.png'}
+        ];
+
+
+        //navigate to specific news
+        $scope.navigateToNews = function (newsItem) {
+            var newsUrl = 'layout/news/news_page_' + newsItem.id + '.html';
+            //save in storage
+            localStorage.setItem("newsSelected", newsUrl);
+            //rootscope to be faster
+            $rootScope.newsSelected = newsUrl;
+            //navigate
+            $location.path('/view2');
         };
 
     }]);
--- www/layout/view2/View2Controller.js
+++ www/layout/view2/View2Controller.js
@@ -1,10 +1,16 @@
 app
-    .controller('View2Controller', ['$scope', '$location', function($scope, $location) {
+    .controller('View2Controller', ['$scope', '$location','$rootScope', function($scope, $location,$rootScope) {
 
-        $scope.navigate = function (path) {
-            $location.path(path);
-        };
+        //try to get on rootscope
+        $scope.newsSelected = $rootScope.newsSelected;
+
+        //try to get on storage
+        if($scope.newsSelected === undefined)
+            $scope.newsSelected = localStorage.getItem("newsSelected");
+
+        //fallback - in case of failure
+        if($scope.newsSelected === undefined || $scope.newsSelected === null)
+            $location.path('/home');
+
 
     }]);
-
-
